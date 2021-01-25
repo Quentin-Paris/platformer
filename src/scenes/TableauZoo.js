af0ec3bc1fb14f49e489f9fbf61909697fa15d6a
Quentin Paris <72392097+Quentin-Paris@users.noreply.github.com>
2021-01-25 14:47:43 +0100

    test zoo
    Zoo en premier

--- src/scenes/TableauZoo.js
+++ src/scenes/TableauZoo.js
@@ -1,31 +1,39 @@
-class Tableau00 extends Tableau{
+class Tableau06 extends Tableau{
 
     preload() {
         super.preload();
         this.load.image('star', 'assets/star.png');
+        this.load.image('monster-violet', 'assets/monster-violet.png');
+
     }
     create() {
         super.create();
+        //quelques étoiles
+        let largeur=64*2;
+        this.stars=this.physics.add.group();
+        for(let posX=largeur/2;posX<largeur*7;posX+=largeur){
+            this.stars.create(posX ,0,"star");
+        }
+        this.stars.children.iterate(function (child) {
+            child.setBounce(1);
+            child.setGravity(1);
+            child.setCollideWorldBounds(true);
+            child.setVelocity( 0,Phaser.Math.Between(-100, 100));
+            child.setMaxVelocity(0,500);
+        });
+        this.physics.add.overlap(this.player, this.stars, this.ramasserEtoile, null, this);
 
-        //des étoiles
-        this.star1=this.physics.add.sprite(200,100,"star");
-        this.star1.setCollideWorldBounds(true);
-        this.star1.setBounce(0);
+        //notre monstre
+        this.monstre=this.physics.add.sprite(300,this.sys.canvas.height-100,"monster-violet");
+        this.monstre.setOrigin(0,0);
+        this.monstre.setDisplaySize(64,64);
+        this.monstre.setCollideWorldBounds(true);
+        this.monstre.setBounce(1);
+        this.monstre.setVelocityX(50);
+        this.physics.add.overlap(this.player, this.monstre, this.hitSpike, null, this);
 
-        this.star2=this.physics.add.sprite(400,100,"star");
-        this.star2.setCollideWorldBounds(true);
-        this.star2.setBounce(0.5);
 
-        this.star3=this.physics.add.sprite(600,100,"star");
-        this.star3.setCollideWorldBounds(true);
-        this.star3.setBounce(1);
-
-        //quand le joueur touche une étoile on appelle la fonction ramasserEtoile
-        this.physics.add.overlap(this.player, this.star1, this.ramasserEtoile, null, this);
-        this.physics.add.overlap(this.player, this.star2, this.ramasserEtoile, null, this);
-        this.physics.add.overlap(this.player, this.star3, this.ramasserEtoile, null, this);
 
     }
 
 }
-
