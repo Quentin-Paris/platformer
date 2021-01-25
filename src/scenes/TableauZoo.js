952029f638815340244f18e263118f98d2be67a9
Quentin Paris <72392097+Quentin-Paris@users.noreply.github.com>
2021-01-25 15:23:27 +0100

    monstre rose
    ajout du monstre ressort rose

--- src/scenes/TableauZoo.js
+++ src/scenes/TableauZoo.js
@@ -4,7 +4,7 @@ class Tableau06 extends Tableau{
         super.preload();
         this.load.image('star', 'assets/star.png');
         this.load.image('monster-violet', 'assets/monster-violet.png');
-
+        this.load.image('monstre-rose','assets/monstre-rose.png');
     }
     create() {
         super.create();
@@ -23,16 +23,24 @@ class Tableau06 extends Tableau{
         });
         this.physics.add.overlap(this.player, this.stars, this.ramasserEtoile, null, this);
 
-        //notre monstre
-        this.monstre=this.physics.add.sprite(300,this.sys.canvas.height-100,"monster-violet");
+        //notre monstre violet
+        this.monstre=this.physics.add.sprite(300,this.sys.canvas.height-65,"monster-violet");
         this.monstre.setOrigin(0,0);
         this.monstre.setDisplaySize(64,64);
         this.monstre.setCollideWorldBounds(true);
         this.monstre.setBounce(1);
-        this.monstre.setVelocityX(50);
+        this.monstre.setVelocityX(40);
         this.physics.add.overlap(this.player, this.monstre, this.hitSpike, null, this);
 
-
+        //notre monstre rose
+    
+        this.monstre=this.physics.add.sprite(300,this.sys.canvas.height-150,"monstre-rose");
+        this.monstre.setOrigin(0,0);
+        this.monstre.setDisplaySize(43,32);
+        this.monstre.setCollideWorldBounds(true);
+        this.monstre.setBounce(1);
+        this.monstre.setVelocityX(75);
+        this.physics.add.overlap(this.player, this.monstre, this.hitSpike, null, this);
 
     }
 
